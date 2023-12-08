9b1d2012dcd2465c8e066cc5510c01e27f0bccf3
lucas <lucas.debouche@utbm.fr>
2023-12-08 13:22:14 +0100

    files-list.c

--- files-list.c
+++ files-list.c
@@ -61,7 +61,7 @@ files_list_entry_t *add_file_entry(files_list_t *list, char *file_path) {
  * @return 0 in case of success, -1 else
  */
 int add_entry_to_tail(files_list_t *list, files_list_entry_t *entry) {
-    files_list_entry_t tail = (files_list_entry_t *) malloc(sizeof(files_list_entry_t));
+    files_list_entry_t *tail = (files_list_entry_t *) malloc(sizeof(files_list_entry_t));
     tail->path_and_name = entry->path_and_name;
     tail->mtime = entry->mtime;
     tail->size = entry->size;
