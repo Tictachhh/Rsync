712f8243755c0a19d6a23712132121d0cf48f2ad
Thibtin <151622620+Thibtin@users.noreply.github.com>
2023-12-30 12:22:27 +0100

    Update configuration.c

--- configuration.c
+++ configuration.c
@@ -32,6 +32,15 @@ void init_configuration(configuration_t *the_config) {
     the_config->uses_md5 = true;
     the_config->verbose = false;
     the_config->dry_run = false;
+
+    // Vérification des paramètres
+    if (the_config->processes_count < 1) {
+        the_config->processes_count = 1; // Au moins 1 processus
+    }
+
+    // Par défaut, MD5 et parallélisme sont actifs
+    the_config->uses_md5 = true;
+    the_config->is_parallel = true;
 }
 
 /*!
@@ -66,39 +75,56 @@ int set_configuration(configuration_t *the_config, int argc, char *argv[]) {
 
     int opt = 0;
     struct option my_opts[] = {
-            {.name = "date-size-only", .has_arg = 0, .flag = 0, .val = 'a'},
-            {.name = "n", .has_arg = 1, .flag = 0, .val = 'b'},
-            {.name = "no-parallel", .has_arg = 0, .flag = 0, .val = 'c'},
-            {.name = "v", .has_arg = 0, .flag = 0, .val = 'd'},
-            {.name = "dry-run", .has_arg = 0, .flag = 0, .val = 'e'},
-            {.name = 0, .has_arg = 0, .flag = 0, .val = 0},
+        {.name = "date-size-only", .has_arg = 0, .flag = 0, .val = 'a'},
+        {.name = "n", .has_arg = 1, .flag = 0, .val = 'b'},
+        {.name = "no-parallel", .has_arg = 0, .flag = 0, .val = 'c'},
+        {.name = "v", .has_arg = 0, .flag = 0, .val = 'd'},
+        {.name = "dry-run", .has_arg = 0, .flag = 0, .val = 'e'},
+        {.name = 0, .has_arg = 0, .flag = 0, .val = 0},
     };
+
     while ((opt = getopt_long(argc, argv, "", my_opts, NULL)) != -1) {
         switch (opt) {
-            case 'a' :
+            case 'a':
                 the_config->uses_md5 = false;
                 break;
 
-            case 'b' :
+            case 'b':
                 if (optarg) {
                     the_config->processes_count = atoi(optarg);
                 }
                 break;
 
-            case 'c' :
+            case 'c':
                 the_config->is_parallel = false;
                 break;
 
-            case 'd' :
+            case 'd':
                 the_config->verbose = true;
                 break;
 
-            case 'e' :
+            case 'e':
                 the_config->dry_run = true;
                 break;
         }
     }
-    return 0;
 
+    // Vérification des arguments restants
+    if (optind + 1 >= argc) {
+        fprintf(stderr, "Erreur: Il manque des arguments.\n");
+        return -1;
+    }
+
+    // Vérification des tailles des chaînes de caractères
+    if (strlen(argv[optind]) >= 1024 || strlen(argv[optind + 1]) >= 1024) {
+        fprintf(stderr, "Erreur: Les chemins sources et de destination doivent avoir une taille inférieure à 1024 caractères.\n");
+        return -1;
+    }
+
+    // Copie des chemins source et destination
+    strcpy(the_config->source, argv[optind]);
+    strcpy(the_config->destination, argv[optind + 1]);
+
+    return 0;
 
 }
