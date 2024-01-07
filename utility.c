4bbe91807cd4c42ce4f6ee6763c1d50509026aa2
zeflo39 <151622454+zeflo39@users.noreply.github.com>
2024-01-07 22:38:37 +0100

    Update utility.c

--- utility.c
+++ utility.c
@@ -14,7 +14,7 @@
  */
 char *concat_path(char *result, char *prefix, char *suffix) {
 
-    if(strlen(prefix) + strlen(suffix) + 2 > PATH_SIZE){
+    if (strlen(prefix) + strlen(suffix) + 2 > PATH_SIZE) {
         return NULL;
     }
     result = malloc((strlen(prefix) + strlen(suffix) + 2) * sizeof (char));
